ac4adcf88300389408feb9cec31d849749d6a2c5
toraOage19 <toraoage19@gmail.com>
2021-11-06 09:31:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Manchester United</p>
+      <input type="checkbox" />
+      <p>Villareal</p>
+      <input type="checkbox" />
+      <p>Atalanta</p>
+      <input type="checkbox" />
+      <p>Young boys</p>
+    </div>
+  );
+}
+
+export default App;
